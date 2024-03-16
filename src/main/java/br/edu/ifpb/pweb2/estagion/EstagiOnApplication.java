75d29c4b244af41dccdedbf1845b9903c811d0b1
paulosys <paulosilvasergio486@gmail.com>
2024-03-15 23:20:20 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/pweb2/estagion/EstagiOnApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.estagion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagiOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstagiOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpb/pweb2/estagion/EstagiOnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.estagion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagiOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
