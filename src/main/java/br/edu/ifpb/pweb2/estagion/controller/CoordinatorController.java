62a6f03d1fe17bf21f91a04664b4d3d445421d1f
ClodsNegreiros <clodsn@gmail.com>
2024-08-27 02:45:07 -0300

    feat: Creating endpoints to list aplicattion by status, view especific company/student

--- src/main/java/br/edu/ifpb/pweb2/estagion/controller/CoordinatorController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagion/controller/CoordinatorController.java
@@ -1,14 +1,31 @@
 package br.edu.ifpb.pweb2.estagion.controller;
 
-import br.edu.ifpb.pweb2.estagion.model.Coordinator;
+import br.edu.ifpb.pweb2.estagion.model.*;
+import br.edu.ifpb.pweb2.estagion.service.ApplicationService;
+import br.edu.ifpb.pweb2.estagion.service.CompanyService;
+import br.edu.ifpb.pweb2.estagion.service.InternshipOfferService;
+import br.edu.ifpb.pweb2.estagion.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/coordinator")
 public class CoordinatorController {
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Autowired
+    private StudentService studentService;
+
 
     @GetMapping
     public ModelAndView showHome(
@@ -18,6 +35,39 @@ public class CoordinatorController {
         modelAndView.setViewName("coordinator/index");
         modelAndView.addObject("coordinator", coordinator);
 
+        return modelAndView;
+    }
+
+    @GetMapping("/list-application")
+    public ModelAndView listApplications(
+            ModelAndView modelAndView
+    ) {
+        List<Application> applications = applicationService.findAllByStauts(EApplicationStatus.APPLIED);
+
+        modelAndView.setViewName("coordinator/list-application");
+        modelAndView.addObject("applications", applicationService.findAllByStauts(EApplicationStatus.APPLIED));
+        return modelAndView;
+    }
+
+    @GetMapping("/view-company")
+    public ModelAndView showCompany(
+            @RequestParam("companyId") Integer companyId,
+            ModelAndView modelAndView
+    ) {
+        modelAndView.setViewName("coordinator/view-company");
+
+        modelAndView.addObject("company", companyService.findById(companyId));
+        return modelAndView;
+    }
+
+    @GetMapping("/view-student")
+    public ModelAndView showStudent(
+            @RequestParam("studentId") Integer studentId,
+            ModelAndView modelAndView
+    ) {
+        modelAndView.setViewName("coordinator/view-student");
+
+        modelAndView.addObject("student", studentService.findById(studentId));
         return modelAndView;
     }
 }
