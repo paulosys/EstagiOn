b0d2b541a4fde28cf3dc6c02cfc572b2ef58cc4b
paulosys <paulosilvasergio486@gmail.com>
2024-09-30 23:20:11 -0300

    feat: Add password encoder

--- src/main/java/br/edu/ifpb/pweb2/estagion/auth/SecurityConfig.java
+++ src/main/java/br/edu/ifpb/pweb2/estagion/auth/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,4 +29,10 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
