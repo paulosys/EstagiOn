edaa5c5f90a07002ebaac379c0aa46ce8e6398a4
paulosys <paulosilvasergio486@gmail.com>
2024-09-30 22:46:42 -0300

    feat: Add security initial configs

---
+++ src/main/java/br/edu/ifpb/pweb2/estagion/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.edu.ifpb.pweb2.estagion.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/").permitAll()
+                        .requestMatchers("/coordinator/**").hasRole("ADMIN")
+                        .requestMatchers("/company/**").hasRole("COMPANY")
+                        .requestMatchers("/student/**").hasRole("STUDENT")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
