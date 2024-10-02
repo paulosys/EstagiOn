2410de1bb0d4d4e200617ec97f2f6c4e1d701c14
paulosys <paulosilvasergio486@gmail.com>
2024-10-02 19:20:03 -0300

    fix: Fix all coordinator bugs

--- src/main/java/br/edu/ifpb/pweb2/estagion/controller/CoordinatorController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagion/controller/CoordinatorController.java
@@ -5,6 +5,7 @@ import br.edu.ifpb.pweb2.estagion.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,10 +25,10 @@ public class CoordinatorController {
     private StudentService studentService;
 
     @Autowired
-    private InternshipOfferService _internshipOfferService;
+    private InternshipOfferService internshipOfferService;
 
     @Autowired
-    private StatusInternshipOfferService _statusInternshipOfferService;
+    private StatusInternshipOfferService statusInternshipOfferService;
 
     @GetMapping
     public ModelAndView showHome(
@@ -48,8 +49,7 @@ public class CoordinatorController {
         List<Application> applications = applicationService.findAllByStauts(EApplicationStatus.APPLIED);
 
         modelAndView.setViewName("coordinator/list-application");
-        modelAndView.addObject("applications", applicationService.findAllByStauts(EApplicationStatus.APPLIED));
-        modelAndView.addObject("logoutUrl", "/auth/coordinator/login");
+        modelAndView.addObject("applications", applications);
         return modelAndView;
     }
 
@@ -77,23 +77,22 @@ public class CoordinatorController {
         return modelAndView;
     }
 
-    @GetMapping("/view-offer")
+    @GetMapping("/view-offer/{id}")
     public ModelAndView showoffer(
-            @RequestParam("offerId") Integer offerId,
+            @PathVariable("id") Integer id,
             ModelAndView modelAndView
     ) {
         modelAndView.setViewName("coordinator/view-offer");
 
-        modelAndView.addObject("offer", _internshipOfferService.findById(offerId));
-        modelAndView.addObject("logoutUrl", "/auth/coordinator/login");
+        modelAndView.addObject("offer", internshipOfferService.findById(id));
         return modelAndView;
     }
 
     @GetMapping("/get-all-internship-offers")
     public ModelAndView GetAllInternshipOffers(ModelAndView modelAndView) {
-        StatusInternshipOffer statusInternshipOffer = _statusInternshipOfferService.findById(1);
+        StatusInternshipOffer statusInternshipOffer = statusInternshipOfferService.findById(1);
 
-        List<InternshipOffer> internshipOffers = _internshipOfferService.findByStatus(statusInternshipOffer);
+        List<InternshipOffer> internshipOffers = internshipOfferService.findByStatus(statusInternshipOffer);
 
         if (internshipOffers == null || internshipOffers.isEmpty()) {
             System.out.println("Nenhuma oferta de estágio encontrada!");
